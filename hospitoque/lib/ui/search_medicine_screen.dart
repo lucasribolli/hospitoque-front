af1ddf00322e5dfe76ab92b0fa4afbb318feb2d2
lucasribolli <lucasribolli@gmail.com>
2022-11-06 20:11:14 -0300

    add list view on medicines result list

--- hospitoque/lib/ui/search_medicine_screen.dart
+++ hospitoque/lib/ui/search_medicine_screen.dart
@@ -54,63 +54,73 @@ class _Medicines extends StatelessWidget {
         child: medicines.isNotEmpty
             ? Column(
                 children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        flex: _columnFlex,
-                        child: Text(
-                          'ID',
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Expanded(
-                        flex: _columnFlex,
-                        child: Text(
-                          'Nome',
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Expanded(
-                        flex: _columnFlex,
-                        child: Text(
-                          'Un. Disp.',
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Divider(),
-                  ...medicines
-                      .map(
-                        (m) => GestureDetector(
-                          onTap: () => _onMedicineTap(context, m),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Expanded(
-                                flex: _columnFlex,
-                                child: _TableItem(
-                                  value: m.id!,
-                                ),
-                              ),
-                              Expanded(
-                                flex: _columnFlex,
-                                child: _TableItem(
-                                  value: m.name,
-                                ),
-                              ),
-                              Expanded(
-                                flex: _columnFlex,
-                                child: _TableItem(
-                                  value: m.available.toString(),
-                                ),
-                              )
-                            ],
+                  Flexible(
+                    flex: 2,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          flex: _columnFlex,
+                          child: Text(
+                            'ID',
+                            textAlign: TextAlign.center,
                           ),
                         ),
-                      )
-                      .toList()
+                        Expanded(
+                          flex: _columnFlex,
+                          child: Text(
+                            'Nome',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Expanded(
+                          flex: _columnFlex,
+                          child: Text(
+                            'Un. Disp.',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  Flexible(
+                    flex: 9,
+                    child: ListView(
+                      children: [
+                        ...medicines
+                            .map(
+                              (m) => GestureDetector(
+                                onTap: () => _onMedicineTap(context, m),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: [
+                                    Expanded(
+                                      flex: _columnFlex,
+                                      child: _TableItem(
+                                        value: m.id!,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: _columnFlex,
+                                      child: _TableItem(
+                                        value: m.name,
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: _columnFlex,
+                                      child: _TableItem(
+                                        value: m.available.toString(),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ],
+                    ),
+                  )
                 ],
               )
             : const _EmptyListState(),
